bae11403c730cd0a7b2b87cae094d629cddf5402
samir <sa.samir213002019@gmail.com>
2025-11-12 05:50:19 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+
+let name = "samir";
+console.log(name);
+
+
+//sum two number
+
+
+let a = 4;
+let b = 8;
+let sum=a+b;
+console.log(sum);
+
+
+// check even or odd
+
+
+let Number=13;
+
+
+if(Number %2 === 0)
+{
+
+    console.log("this number is even");
+}
+
+else{
+    console.log("odd");
+}
